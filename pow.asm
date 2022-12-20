a1f810999118c3ed6ee52e540dc589ae3a2d47f8
iTzFabregas <fabricio-sampaio@usp.br>
2022-12-20 10:54:43 -0300

    final changes

--- pow.asm
+++ pow.asm
@@ -12,13 +12,13 @@ main:
 	loadn r0, #2
 	loadn r1, #5 ; 32
 	pow r0, r0, r1
-	loadn r1, #'X'
-	outchar r1, r0
+	loadn r1, #'Y'
+	outchar r1, r0	
 
 	loadn r0, #10
 	loadn r1, #4 ; 10000
 	pow r0, r0, r1
-	loadn r1, #'X'
+	loadn r1, #'Z'
 	outchar r1, r0
 
 	halt
